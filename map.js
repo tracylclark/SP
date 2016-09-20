1200d9dfa67d9cc76a3bdde6aed436e070565a7b
Tracy Clark <Tracy Clark>
2016-09-19 21:56:57 -0400

    finished initializingTiles function, modified the Tile constructor to reflect creation by the initializeTile function, implemented the shuffle function for tiles, and made changes to array filling for vertice creation to correct a mistake in implementation. Also cleaned up naming conventions for clarity and uniformity.

--- map.js
+++ map.js
@@ -1,17 +1,18 @@
 //map.js
 
 module.exports = function(){
-		const this.defaultTiles = ["storage","RAM","CPU","bandwidth","storage","RAM","RAM","power","bandwidth","CPU","power","power","bandwidth","RAM","CPU","power","storage","CPU","DarkNet"];
+		const defaultTileLabels = ["storage","RAM","CPU","bandwidth","storage","RAM","RAM","power","bandwidth","CPU","power","power","bandwidth","RAM","CPU","power","storage","CPU","DarkNet"];
 		this.init = function(options){
 			var mapType = options.mapType;
 			var gameMap = new GameMap(mapType);
-			initializeTileTypes(mapType);
-
 		}
 		function GameMap(mapType){
-			this.vertices = new Array(11).fill(new Array(6).fill(0)); //create the vertices and fill with 0's
+			this.vertices = new Array(11).fill(0).map((e)=>new Array(6).fill(0)); //create the vertices and fill with 0's
 			this.vertices.forEach((e,i)=>e.map((e,j)=>new Vertex({x:i,y:j}))); //this fills with base vertices, which holds references to Tiles/Edges
-		}
+			this.tiles = initializeTiles(mapType);
+          	this.edges = [];
+            //use adjacency object to link tiles, vertices, and edges
+        }
 		function Vertex(coords){
 			var invalidCoords = [
 				{x:0, y:0}, {x:1, y:0}, {x:0, y:1}, {x:9, y:0}, {x:10, y:0}, {x:10, y:1},
@@ -22,31 +23,31 @@ module.exports = function(){
 			if(invalidCoords.find((e)=>this.coords.x == e.x && this.coords.y == e.y)){
 				this.accessible = false; //invalid coords are not accessible
 			}
-			this.tiles = ; //array of 18 tiles
-			this.edges = ;
+			this.tiles = []; //array of 18 tiles
+			this.edges = [];
 		}
 		function Edge(left,right){
 			this.left = left;
 			this.right = right;
 			this.road = null; //when a player places a road, it is created and contained in the edge object
 		}
-		function Tile(id){
+		function Tile(id, type){
 			this.id = id;
 			this.number = null;
-			this.resource = null;
-			this.robber = false;
+			this.resource = type;
+			this.robber = type==="DarkNet";
 		}
-		function initializeTiles(mapType, tileObjects){
-			var this.tiles = defaultTiles; 
+		function initializeTiles(mapType){
+			var tileTypeBuffer = defaultTileLabels; 
 			if(mapType != "default"){
+				var shuffledTileLabels = [], i; 
 				//randomize tiles if not a default (beginner) map, use a Knuth Shuffle (one of the variants)
-			}
-			for(var i=0; i<19; i++){
-				//populate the tiles 
-				if(tiles[i]=="DarkNet"){
-					tileObjects[i].robber = true;
+				while (tileTypeBuffer.length){
+					i = Math.floor(Math.random() * tileTypeBuffer.length);
+					shuffledTileLabels.push(tileTypeBuffer.splice(i, 1));
 				}
-				tileObjects[i].resource = tiles[i];
+				tileTypeBuffer = shuffledTileLabels;
 			}
+			return tileTypeBuffer.map((e,i)=> new Tile(i,e));
 		}
 };
