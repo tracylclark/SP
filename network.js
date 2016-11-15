d3a9c47c0464cffb2e5ce0ed642259855a95034f
Tracy Clark <Tracy Clark>
2016-11-15 00:07:51 -0500

    Added client.js code for getting the map object from the server and rendering it.

--- network.js
+++ network.js
@@ -1,26 +1,39 @@
 //network.js
 
-module.exports = function(io, players){
+module.exports = function(io){//, players){
 	//handles all of the network traffic
-	io.on("connect", function(socket) {
-		socket.on("disconnect", function(socket){
+	console.log("I'm a network");
+	//to remove, just for testing purposes all map references
+	var Map = new require('./map.js');
+	var m = new Map({
+		tileDistribution:"default", //other
+		tokenDistribution:"default", //"random"
+		vendorDistribution:"default", //"slightShift" //other
+	});
+	io.on("connect", socket=>{
+		console.log("Client connected.");
+		socket.on("msg", msg=>console.log(msg));
+		socket.on("gimmeMap", ()=>{
+			console.log("map request");
+			socket.emit("gameMap", m.getSerializedMap());
+		});
+		socket.on("disconnect", socket=>{
 
 		});
-		socket.on("startGame", function(options){
+		socket.on("startGame", options=>{
 
 		});
-		socket.on("login", function(credentials){
+		socket.on("login", credentials=>{
 
 		});
-		socket.on("createAccount", function(credentials){
+		socket.on("createAccount", credentials=>{
 
 		});
-		socket.on("buildRoad", function(edgeCoords){
+		socket.on("buildRoad", edgeCoords=>{
 
 		});
 	});
     //this.init = function(options){
 
     //}
-
 };
