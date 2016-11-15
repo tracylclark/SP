24c9a053b438fa46d97dd4e93236ea9a661d925a
Tracy Clark <Tracy Clark>
2016-11-14 23:12:36 -0500

    Updated server.js from deprecated sendfile() to updated sendFile() as well as adding a handler for page requests other than root directory.

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 //server.js
 
 (function(){
-	var players = [];
-
-	var Player = require("./player.js"); 
-	var map = new (require("./map.js"))();
 	var expressServer = require('express')();
-	var http = require('http').Server(expressServer);
+	var http = require('http').createServer(expressServer);
 	var io = require('socket.io')(http);
 	var myNetwork = require('./network.js');
-	var network = new myNetwork(io, players); 
-	expressServer.get('/', (req, res)=> res.sendfile('pub\index.html') );
-
-	map.init();
-	network.init();
-	http.listen(8084, ()=> console.log('listening on *:8084') );
-
+	var network = new myNetwork(io);
+	expressServer.get('/', (req, res)=> res.sendFile(__dirname + '/pub/index.html')); //updated from deprecated sendfile()
+	expressServer.get(/^(.+)$/, function(req, res){ //define a path for any requests other than root domain
+        console.log('static file request : ' + req.params);
+        res.sendFile( __dirname + "/pub" + req.params[0]);
+    });
+	http.listen(8004, ()=> console.log('listening on *:8004') );
 }());
