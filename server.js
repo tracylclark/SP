35e07b97e45814cefbc0ac26a00614ad305dfa39
Tracy Clark <Tracy Clark>
2016-11-16 22:44:16 -0500

    Updates to how server, network, and gameEngine interact.

--- server.js
+++ server.js
@@ -4,12 +4,18 @@
 	var expressServer = require('express')();
 	var http = require('http').createServer(expressServer);
 	var io = require('socket.io')(http);
-	var myNetwork = require('./network.js');
-	var network = new myNetwork(io);
+	var Network = require('./network.js');
+	var GameEngine = require('./gameEngine.js');
+	var network = new Network(io);
+	var gameEngine = new GameEngine();
+	var players = [];
+	network.init(gameEngine, players);
+	gameEngine.init(network, players);
+
 	expressServer.get('/', (req, res)=> res.sendFile(__dirname + '/pub/index.html')); //updated from deprecated sendfile()
 	expressServer.get(/^(.+)$/, function(req, res){ //define a path for any requests other than root domain
-        console.log('static file request : ' + req.params);
-        res.sendFile( __dirname + "/pub" + req.params[0]);
-    });
+		console.log('static file request : ' + req.params);
+		res.sendFile( __dirname + "/pub" + req.params[0]);
+	});
 	http.listen(8004, ()=> console.log('listening on *:8004') );
 }());
