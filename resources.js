934403605ddabb095b6bd04f3c0e7e2b637c5219
Tracy Clark <Tracy Clark>
2016-11-17 22:01:25 -0500

    Created functions for adding and removing resources as well as an initialization of all resources to zero.

--- resources.js
+++ resources.js
@@ -1,9 +1,23 @@
 //resources.js
-
 module.exports = function(){
-
-    this.init = function(options){
-
-    }
-
-};
+	
+	this.bandwidth = 0;
+	this.storage = 0;
+	this.cpu =  0;
+	this.ram = 0;
+	this.power = 0;
+	this.add = function(res){
+		this.bandwidth += res.bandwidth;
+		this.storage += res.storage;
+		this.cpu += res.cpu;
+		this.ram += res.ram;
+		this.power += res.power;
+	}
+	this.sub = function(res){
+		this.bandwidth -= res.bandwidth;
+		this.storage -= res.storage;
+		this.cpu -= res.cpu;
+		this.ram -= res.ram;
+		this.power -= res.power;
+	}
+}
