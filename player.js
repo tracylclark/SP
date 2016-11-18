a84dc048f6259295e76e3fa3780693e7f0af411c
Tracy Clark <Tracy Clark>
2016-11-17 22:02:12 -0500

    Created initialization for player objects to hold all player traits.

--- player.js
+++ player.js
@@ -1,9 +1,17 @@
 //player.js
 
-module.exports = function(){
-
-    this.init = function(options){
-
-    }
-
+module.exports = function(player, color){
+	this.socket = player.socket;
+	this.username = player.username;
+	this.color = color;
+	this.resources = new require("/.resources.js")();
+	this.victoryPoints = [];
+	this.developments = [];
+	this.infrastructure = {
+		servers: [],
+		databases: [],
+		networks: []
+	};
+	this.vendors = [];
+	this.whiteHats = 0;
 };
