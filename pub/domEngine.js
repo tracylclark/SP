e9def6e67acdc73e02f391c3721a059d669fef72
Tracy Clark <Tracy Clark>
2016-11-25 13:13:59 -0500

    Implemented system messages to be displayed to the user

--- pub/domEngine.js
+++ pub/domEngine.js
@@ -1,64 +1,67 @@
 //domEngine.js
 
 var domEngine = new (function(){
-    var dom = {};
-    var get = (f)=>{return document.getElementById(f)};
-    this.init = function(){
-        dom = {
-            overlay:get("overlay"),
-            login:{
-                container:get("loginContainer"),
-                loginButton:get("loginButton"),
-                createButton:get("loginCreateButton"),
-                username:get("loginUsername"),
-                password:get("loginPassword"),
-                error:get("loginError")
-            },
-            chat:{
-                container:get("chatContainer"),
-                viewport:get("chatViewport"),
-                button:get("chatButton"),
-                input:get("chatInput")
-            },
-            resources:{
-                container:get("resourcesContainer"),
-                cpu:get("resourcesCPU"),
-                bandwidth:("resourcesBandwidth"),
-                power:("resourcesPower"),
-                ram:get("resourcesRAM"),
-                storage:get("resourcesStorage")
-            }
-        };
+	var dom = {};
+	var get = (f)=>{return document.getElementById(f)};
+	this.init = function(){
+		dom = {
+			overlay:get("overlay"),
+			login:{
+				container:get("loginContainer"),
+				loginButton:get("loginButton"),
+				createButton:get("loginCreateButton"),
+				username:get("loginUsername"),
+				password:get("loginPassword"),
+				error:get("loginError")
+			},
+			chat:{
+				container:get("chatContainer"),
+				viewport:get("chatViewport"),
+				button:get("chatButton"),
+				input:get("chatInput")
+			},
+			resources:{
+				container:get("resourcesContainer"),
+				cpu:get("resourcesCPU"),
+				bandwidth:("resourcesBandwidth"),
+				power:("resourcesPower"),
+				ram:get("resourcesRAM"),
+				storage:get("resourcesStorage")
+			}
+		};
 
-        dom.login.loginButton.onclick = ()=>{
-            network.login(dom.login.username.value, dom.login.password.value);
-        };
-        dom.login.createButton.onclick = ()=>{
-            network.create(dom.login.username.value, dom.login.password.value);
-        };
-        dom.chat.button.onlick = ()=>{
-            network.broadcast(dom.chat.input.value); //clean on the server side
-        }
+		dom.login.loginButton.onclick = ()=>{
+			network.login(dom.login.username.value, dom.login.password.value);
+		};
+		dom.login.createButton.onclick = ()=>{
+			network.create(dom.login.username.value, dom.login.password.value);
+		};
+		dom.chat.button.onlick = ()=>{
+			network.broadcast(dom.chat.input.value); //clean on the server side
+		}
 
-    }
-    this.hideLogin =function(){
-        dom.login.container.style.display = "none";
-        dom.resources.container.style.visibility = "visible";
-        dom.chat.container.style.visibility = "visible";
-    }
-    this.loginError = function(msg){
-        dom.login.error.innerHTML = msg;
-        dom.login.error.style.visibility = "visible";
-    }
-    this.chatDisplay = function(msg){
-        dom.chat.viewport.innerHTML +="\n"+msg; //playerName :: msg
-        dom.chat.viewport.scrollTop = dom.chat.viewport.scrollHeight; //always scroll to bottom of chat window
-    }
-    this.resourceUpdate = function(resources){
-        dom.resources.cpu.innerHTML = resources.cpu;
-        dom.resources.bandwidth.innerHTML = resources.bandwidth;
-        dom.resources.power.innerHTML = resources.power;
-        dom.resources.ram.innerHTML = resources.ram;
-        dom.resources.storage.innerHTML = resources.storage;
-    }
+	}
+	this.hideLogin =function(){
+		dom.login.container.style.display = "none";
+		dom.resources.container.style.visibility = "visible";
+		dom.chat.container.style.visibility = "visible";
+	}
+	this.loginError = function(msg){
+		dom.login.error.innerHTML = msg;
+		dom.login.error.style.visibility = "visible";
+	}
+	this.chatDisplay = function(msg){
+		dom.chat.viewport.innerHTML +="<br>"+msg; //playerName :: msg
+		dom.chat.viewport.scrollTop = dom.chat.viewport.scrollHeight; //always scroll to bottom of chat window
+	}
+	this.systemMessage = function(msg){
+	  domEngine.chatDisplay("<div style='color:#323232;text-align:center;font-style:italic'>"+msg+"</div");
+	}
+	this.resourceUpdate = function(resources){
+		dom.resources.cpu.innerHTML = resources.cpu;
+		dom.resources.bandwidth.innerHTML = resources.bandwidth;
+		dom.resources.power.innerHTML = resources.power;
+		dom.resources.ram.innerHTML = resources.ram;
+		dom.resources.storage.innerHTML = resources.storage;
+	}
 })();
--- pub/network-client.js
+++ pub/network-client.js
@@ -29,70 +29,90 @@ var network = new (function(){
 		domEngine.chatDisplay(msg);
 	});
 	socket.on("system", msg=>{
-		o(`<system> ${msg}`);
+		domEngine.systemMessage(msg);
 	});
+
 	socket.on("actionSuccess", successMsg=>{
 		o(`<actionSuccess> ${successMsg}`);
+		if(DEBUG)domEngine.systemMessage("actionSuccess:"+msg);
+		if(!successMsg){
+				domEngine.systemMessage("action failed!");
+		}
 	});
 	socket.on("playerUpdate", playerArray=>{
 		o(`<playerUpdate> ${playerArray}`);
 		canvasEngine.setPlayers(playerArray);
+		domEngine.setPlayers(player)
 	});
-    socket.on("resourceUpdate", (resources)=>{
-    	o(`<resourceUpdate> ${resources}`);
-    	domEngine.resourceUpdate(resources);
-    });
-    socket.on("tradeOffer", (offer)=>{
-    	o(`<tradeOffer> ${offer}`);
-    });
-    socket.on("hacked", (hackerResults)=>{
-    	o(`<hacked> ${hackerResults}`)
-    });
-    socket.on("buyPhase", (currentPlayer)=>{
-    	o(`<buyPhase> ${currentPlayer}`);
-    });
-    socket.on("offerRescinded", ()=>{
-    	o(`<offerRescinded>`);
-    });
-    socket.on("tradeRejected", (rejectingPlayer)=>{
-    	o(`<tradeRejected> ${rejectingPlayer}`);
-    });
-    socket.on("tradeComplete", (acceptingPlayer)=>{
-    	o(`<tradeComplete> ${acceptingPlayer}`);
-    });
-    socket.on("tradePhase", (currentPlayer)=>{
-    	o(`<tradePhase> ${currentPlayer}`);
-    });
-    socket.on("gameTurn", (currentPlayer)=>{
-    	o(`<gameTurn> ${currentPlayer}`);
-    });
-    socket.on("cardDraw", (card)=>{
-    	o(`<cardDraw> ${card}`);
-    });
-    socket.on("setupBuild", (currentPlayer)=>{
-    	o(`<setupBuild> ${currentPlayer}`);
-    });
-    socket.on("rollOffResult", (rollResult)=>{
-    	o(`<rollOffResult> ${rollResult}`);
-    });
-    socket.on("rollOff", (currentPlayer)=>{
-    	o(`<rollOff> ${currentPlayer}`);
-    });
-    socket.on("monopoly", ()=>{
-    	o(`<monopoly>`);
-    });
-    socket.on("networkBuilding", ()=>{
-    	o(`<networkBuilding>`);
-    });
-    socket.on("placeHacker", ()=>{
-    	o(`<placeHacker>`);
-    });
-    socket.on("selectGoodQuarterResources", ()=>{
-    	o(`<selectGoodQuarterResources>`);
-    });
+  socket.on("resourceUpdate", (resources)=>{
+  	o(`<resourceUpdate> ${resources}`);
+  	domEngine.resourceUpdate(resources);
+  });
+  socket.on("tradeOffer", (offer)=>{
+  	o(`<tradeOffer> ${offer}`);
+		domEngine.systemMessage("The current player has made a trade offer.");
+		domEngine.systemMessage(offer.have);
+		domEngine.systemMessage(offer.for);
+  });
+  socket.on("hacked", (hackerResults)=>{
+  	o(`<hacked> ${hackerResults}`)
+		domEngine.systemMessage(hackerResults.hacker + " has targetted " + hackerResults.target + " with a cyberattack.");
+  });
+  socket.on("buyPhase", (currentPlayer)=>{
+		domEngine.systemMessage(currentPlayer + " has entered the buy phase of their turn.");
+  });
+  socket.on("tradePhase", (currentPlayer)=>{
+		domEngine.systemMessage(currentPlayer + " has entered the trade phase of their turn.");
+  });
+  socket.on("offerRescinded", ()=>{
+  	o(`<offerRescinded>`);
+		domEngine.systemMessage("The current offer has been rescinded.");
+  });
+  socket.on("tradeRejected", (rejectingPlayer)=>{
+  	o(`<tradeRejected> ${rejectingPlayer}`);
+		domEngine.systemMessage(rejectingPlayer + " has rejected the trade offer.");
+  });
+  socket.on("tradeComplete", (acceptingPlayer)=>{
+  	o(`<tradeComplete> ${acceptingPlayer}`);
+		domEngine.systemMessage(acceptingPlayer + " has accepted the trade offer!");
+  });
+  socket.on("gameTurn", (currentPlayer)=>{
+		domEngine.systemMessage(currentPlayer + " has begun a new turn.");
+  });
+  socket.on("cardDraw", (card)=>{
+		domEngine.systemMessage("You gained a new development: " + card);
+  });
+  socket.on("setupBuild", (currentPlayer)=>{]
+		domEngine.systemMessage(currentPlayer + " must set up one server and one network.");
+  });
+  socket.on("rollOffResult", (rollResult)=>{
+			domEngine.systemMessage(rollResult.player + " rolled off: (" + rollResult.rollOff.roll1 + "," + rollResult.rollOff.roll1 + ") : " + rollResult.rollOff.total);
+  });
+  socket.on("rollOff", ()=>{
+		domEngine.systemMessage("It is time to roll off to determine player order.");
+  });
+  socket.on("monopoly", ()=>{
+		domEngine.systemMessage("You have played a monopoly card. Please select a resource type.");
+  });
+  socket.on("networkBuilding", ()=>{
+		domEngine.systemMessage("You have played a Network Building card. Place two network connections for free!");
+  });
+	socket.on("placeHacker", ()=>{
+		domEngine.systemMessage("You may now place a hacker and select a target");
+  });
 
-	
-	
+  socket.on("selectGoodQuarterResources", ()=>{
+		domEngine.systemMessage("You have had a good quarter. Select two free resources.");
+  });
+
+
+//spectators
+	this.create = function(un,pw){
+		socket.emit("createAccount", {username:un, password:pw});
+	}
+	this.login = function(un,pw){
+		socket.emit("login", {username:un, password:pw});
+	}
 	this.broadcast = function(msg){
 		socket.emit("msg", `<broadcast> ${msg}`);
 		socket.emit("broadcast", msg);
@@ -101,38 +121,36 @@ var network = new (function(){
 		socket.emit("msg", `<joinGame>`);
 		socket.emit("joinGame");
 	};
+
+//players and setup
 	this.startGame = function(options){
 		socket.emit("msg", `<startGame> ${options}`);
 		socket.emit("startGame", options);
 	};
-	this.buildNetwork = function(location){
-		socket.emit("msg", `<buildNetwork> ${location}`);
-		socket.emit("buildNetwork", location);
-	};
-	this.buildDatabase = function(location){
-		socket.emit("msg", `<buildDatabase> ${location}`);
-		socket.emit("buildDatabase", location);
-	};
-	this.buildServer = function(location){
-		socket.emit("msg", `<buildServer> ${location}`);
-		socket.emit("buildServer", location);
-	};
-	this.buyDevelopment = function(){
-		socket.emit("msg", `<buyDevelopment>`);
-		socket.emit("buyDevelopment");
+	this.rollOff = function(){
+		socket.emit("msg", `<rollOff>`);
+		socket.emit("rollOff");
 	};
 	this.endTurn = function(){
 		socket.emit("msg", `<endTurn>`);
 		socket.emit("endTurn");
 	};
+
+//Game Turn
 	this.rollDice = function(){
 		socket.emit("msg", `<rollDice>`);
 		socket.emit("rollDice");
+	};
+	this.placeHacker = function(hackerAction){
+		socket.emit("msg", `<placeHacker> ${hackerAction}`);
+		socket.emit("placeHacker", hackerAction);
 	};
 	this.playDevelopment = function(card){
 		socket.emit("msg", `<playDevelopment> ${card}`);
 		socket.emit("playDevelopment", card);
 	};
+
+	//trades
 	this.offerTrade = function(offer){
 		socket.emit("msg", `<offerTrade> ${offer}`);
 		socket.emit("offerTrade", offer);
@@ -145,24 +163,31 @@ var network = new (function(){
 		socket.emit("msg", `<tradeResponse> ${response}`);
 		socket.emit("tradeResponse", response);
 	};
-	this.placeHacker = function(hackerAction){
-		socket.emit("msg", `<placeHacker> ${hackerAction}`);
-		socket.emit("placeHacker", hackerAction);
+
+
+	//purchases
+	this.buildNetwork = function(location){
+		socket.emit("msg", `<buildNetwork> ${location}`);
+		socket.emit("buildNetwork", location);
 	};
+	this.buildServer = function(location){
+		socket.emit("msg", `<buildServer> ${location}`);
+		socket.emit("buildServer", location);
+	};
+	this.buyDevelopment = function(){
+		socket.emit("msg", `<buyDevelopment>`);
+		socket.emit("buyDevelopment");
+	};
+	this.buildDatabase = function(location){
+		socket.emit("msg", `<buildDatabase> ${location}`);
+		socket.emit("buildDatabase", location);
+	};
+
+	//special
 	this.claimLongestPath = function(pathList){
 		socket.emit("msg", `<claimLongestPath> ${pathList}`);
 		socket.emit("claimLongestPath", pathList);
 	};
-	this.rollOff = function(){
-		socket.emit("msg", `<rollOff>`);
-		socket.emit("rollOff");
-	};
-	this.create = function(un,pw){
-		socket.emit("createAccount", {username:un, password:pw});
-	}
-	this.login = function(un,pw){
-		socket.emit("login", {username:un, password:pw});
-	}
 
 
 })();
