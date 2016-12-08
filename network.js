e77968b2d5665c20e72568b37f8f9f963a455f2e
Tracy Clark <Tracy Clark>
2016-12-07 19:12:11 -0500

    sys messages

--- network.js
+++ network.js
@@ -123,19 +123,9 @@ function login(s, name){//credentials have been vetted at this point
 	s.on("disconnect", spectatorLeaves);
 	s.on("broadcast", msg=>{
 		if(msg=="batman"){
-			networkIO.emit("broadcast", '
-       _==/          i     i          \\==_
-     /XX/            |\\___/|            \\XX\
-   /XXXX\\            |XXXXX|            /XXXX\
-  |XXXXXX\\_         _XXXXXXX_         _/XXXXXX|
- XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX
-|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
- XXXXXX/^^^^"\\XXXXXXXXXXXXXXXXXXXXX/^^^^^\\XXXXXX
-  |XXX|       \\XXX/^^\\XXXXX/^^\\XXX/       |XXX|
-    \\XX\\       \\X/    \\XXX/    \\X/       /XX/
-       "\\       "      \\X/      "      /"
+			networkIO.emit("broadcast", '  /(_M_)\
+ |       |
+  \(~V~)/
 '); //http://www.asciiworld.com/-Batman,88-.html
 
 		}
